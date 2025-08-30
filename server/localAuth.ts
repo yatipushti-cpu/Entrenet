85cd797dbfb9e34401ed2f88829adb6283d4e27a
yatipushti-cpu <yatipushti@gmail.com>
2025-08-30 14:05:02 +0530

    Fix express-session import error

--- server/localAuth.ts
+++ server/localAuth.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import bcrypt from 'bcryptjs';
-import * as session from 'express-session';
+import session from 'express-session';
 import { v4 as uuidv4 } from 'uuid';
 import { storage } from './localStorage';
 import { env } from 'process';
